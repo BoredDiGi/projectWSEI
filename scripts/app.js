6813c3abe56cd92f4c1737a592528e6ab6aeaef6
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-05-14 14:51:13 +0200

    graphic upgraded and new functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,30 @@
 var x = 0;
 function rec(x) {
     if( x == 1)
-    document.getElementById("text").innerHTML = " przepis 1 ";
+    {
+        //document.getElementById("text").innerHTML = " przepis 1 ";
+        document.getElementById("text").innerHTML = ' <div> <img src="./assets/dumplings.jpg"> przepis 1</div> ';
+    }
     else
     if( x == 2)
-    document.getElementById("text").innerHTML = " przepis 2 ";
+    {
+        //document.getElementById("text").innerHTML = " przepis 2 ";
+        document.getElementById("text").innerHTML = ' <div> <img src="./assets/rest.jpg"> przepis 2</div> ';
+    }
     else
     if( x == 3)
-    document.getElementById("text").innerHTML = " przepis 3 ";
+    {
+        //document.getElementById("text").innerHTML = " przepis 3 ";
+        document.getElementById("text").innerHTML = ' <div> <img src="./assets/salad.jpg"> przepis 3</div> ';
+    }
     else
     document.getElementById("text").innerHTML = " nieobsługiwany warunek, zgłoś go. ";
 
 };
+var v = document.getElementsByTagName('video')[0];
+      
+      v.addEventListener('timeupdate', function()
+      {
+        var p = document.getElementsByTagName('progress')[0];
+        p.value = 100*this.currentTime/this.duration;
+      });
