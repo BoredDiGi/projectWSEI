cdf39111a833ee4dffb24df5659c3e6e40a843c5
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-07-11 08:07:11 +0200

    responsive  pages

--- scripts/app.js
+++ scripts/app.js
@@ -3,22 +3,16 @@ function rec(x) {
     if( x == 1)
     {
         //document.getElementById("text").innerHTML = " przepis 1 ";
-        document.getElementById("text").innerHTML = ' <div> <img src="./assets/dumplings.jpg"> przepis 1</div> ';
+        document.getElementById("text").innerHTML = ' <div>przepis 1</div> ';
     }
     else
     if( x == 2)
     {
         //document.getElementById("text").innerHTML = " przepis 2 ";
-        document.getElementById("text").innerHTML = ' <div> <img src="./assets/rest.jpg"> przepis 2</div> ';
+        document.getElementById("text").innerHTML = ' <div> przepis 2</div> ';
     }
     else
-    if( x == 3)
-    {
-        //document.getElementById("text").innerHTML = " przepis 3 ";
-        document.getElementById("text").innerHTML = ' <div> <img src="./assets/salad.jpg"> przepis 3</div> ';
-    }
-    else
-    document.getElementById("text").innerHTML = " nieobsługiwany warunek, zgłoś go. ";
+    document.getElementById("text").innerHTML = " nieobsługiwany wyjątek, zgłoś go. ";
 
 };
 var video = document.getElementsByTagName('video')[0];
